bfe17933a29d0f838eee5ab05d1f3a71168c5c60
RavenCheng1120 <ravencheng66@gmail.com>
2020-05-17 20:48:29 +0800

    node js passport login started

--- nodePassportLogin/app.js
+++ nodePassportLogin/app.js
@@ -2,10 +2,9 @@
 https://www.youtube.com/watch?v=6FOq4cUdH8k&t=196s
 27:15
  */
-
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // mongoose 是給 Node.js 用的 MongoDB ODM
 
 const app = express();
 
@@ -21,6 +20,8 @@ mongoose.connect(db, { useNewUrlParser: true })
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+// Bodyparser
+app.use(express.urlencoded({ extended: false}));
 
 // Routes
 app.use('/', require('./routes/index'));
--- nodePassportLogin/routes/users.js
+++ nodePassportLogin/routes/users.js
@@ -11,5 +11,47 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
+// Register Handle
+router.post('/register', (req, res) => {
+    let { name, email, password, password2 } = req.body;
+    let errors = [];
+    name = name.trim();
+    email = email.trim();
+    password = password.trim();
+    password2 = password2.trim();
+
+    // check no blank
+    if( !name || !email || !password || !password2){
+        errors.push({ msg: '請填寫全部的資料'} );
+    }
+
+    // check password
+    if( password2 !== password){
+        errors.push({ msg: '密碼檢查錯誤'} );
+    }
+
+    // check password length
+    if( password.length < 6){
+        errors.push({ msg: '密碼長度要至少6個字元'} );
+    }
+
+    const emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!emailRe.test(email)){
+        errors.push({ msg: '電子郵件格式錯誤'} );
+    }
+
+    if( errors.length > 0){
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    }else{
+        res.send('pass');
+    }
+});
+
 
 module.exports = router;
